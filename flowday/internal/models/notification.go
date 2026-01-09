a64eb012a53686bcee4a495ae4d966c65b57825c
SaqO(n*logn) <ONLYPROGRAMMING@Sargiss-MacBook-Air.local>
2026-01-09 21:14:22 +0400

    Correct bug from notification_system

--- flowday/internal/models/notification.go
+++ flowday/internal/models/notification.go
@@ -16,12 +16,13 @@ const (
 )
 
 type Notification struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	UserID    primitive.ObjectID `bson:"user_id" json:"user_id"`
-	Title     string             `bson:"title" json:"title"`
-	Message   string             `bson:"message" json:"message"`
-	Read      bool               `bson:"read" json:"read"`
-	Type      NotificationType   `bson:"type" json:"type"`
-	Link      string             `bson:"link,omitempty" json:"link,omitempty"` // Optional link to task or project
-	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	ID        primitive.ObjectID  `bson:"_id,omitempty" json:"id"`
+	UserID    primitive.ObjectID  `bson:"user_id" json:"user_id"`
+	TaskID    *primitive.ObjectID `bson:"task_id,omitempty" json:"task_id,omitempty"` // Optional link to specific task
+	Title     string              `bson:"title" json:"title"`
+	Message   string              `bson:"message" json:"message"`
+	Read      bool                `bson:"read" json:"read"`
+	Type      NotificationType    `bson:"type" json:"type"`
+	Link      string              `bson:"link,omitempty" json:"link,omitempty"` // Optional link to task or project
+	CreatedAt time.Time           `bson:"created_at" json:"created_at"`
 }
